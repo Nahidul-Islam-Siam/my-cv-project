f6ebbaf4e38262aeca896c7ab731c954b0b0149f
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-01-07 10:24:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+
+  return (
+
+   <div className='container mx-auto max-w-screen-xl'>
+    <Navbar></Navbar>
+   </div>
+  )
+}
+
+export default App
