3b68149de90ab29728be603699729c66a036db15
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-01-07 12:52:31 +0600

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
 
-   <div className='mx-auto '>
+   <div className='mx-auto  max-w-full'>
     {/* <AnnouncedBar></AnnouncedBar> */}
     <Navbar></Navbar>
     <Hero/>
